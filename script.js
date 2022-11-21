bb19669c7955266034a7b10f14cdf61577eee5ee
Jaswanth Kishore S <93701274+JKS46@users.noreply.github.com>
2022-11-21 21:01:03 +0530

    ¯\_(ツ)_/¯

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-/*  */
+const box = document.querySelector("#box>div");
+function createBox(){
+   
+
+}
+function draw(boxRes){
+    
+}
+box.forEach((div) => {
+    div.addEventListener("hover", () => {
+    boxRes = document.getElementById("#input").value;
+    draw(boxRes);
+    });
+});
