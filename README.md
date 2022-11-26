65f711c58c5f42e410156fc79beea5d699afa1b2
Jaswanth Kishore S <93701274+JKS46@users.noreply.github.com>
2022-11-27 04:24:14 +0530

    need to add random and eraser

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Etch a sketch
 
 Website for drawing on a user input x*x res box using hover property
+
+To do
+
+- [ ] add double click enabled brush 
--- script.js
+++ script.js
@@ -1,25 +1,60 @@
 const box = document.querySelector("#box");
-let row = 25,column = 25;
-for(i=0;i<row;i++){
-    for(j=0;j<column;j++){
-        let div = document.createElement('div');
-        div.classList.add('cell');
-        div.style.width = 100/column + '%';
-        div.style.height = 100/row +'%';
-        div.textContent =" ";
-        box.appendChild(div);
+let input = 25;
+/* default row values whn page is loaded */
+let cell= document.querySelectorAll("#box>div");
+let paintColor = "";
+
+createDivs(input,input);
+draw();
+
+/* creates divs inside #box */
+function createDivs(row,column){
+    for(i=0;i<row;i++){
+        for(j=0;j<column;j++){
+            let div = document.createElement('div');
+            div.style.width = 100/column + '%';
+            div.style.height = 100/row +'%';
+            box.appendChild(div);
+        }
     }
 }
-function clearBox(){
-    console.log("clear");
+
+/* resets the color*/
+function reset(){
     cell.forEach((div)=>{
         div.style.backgroundColor = "white";
     });
 }
-const cell = document.querySelectorAll("#box>div");
-cell.forEach((div) => {
-    div.addEventListener("mouseover",()=>{
-        div.style.backgroundColor = "black";
+
+/* creates divs inside #box based on input from user,triggered in html onclick */
+function createBox(){
+    input = document.querySelector("#input").value;
+    if(input<100 && input>0){
+        cell.forEach((div) => {
+            div.removeEventListener('mouseover',paintBlack);
+        });
+       /* clear divs inside box so they don't stack */
+       box.replaceChildren();
+       
+        createDivs(input,input);
+        draw();
+    }
+    /* to limit memory usage */
+    else{
+        alert("Enter a number between 1 and 100");
+    }
+}
+
+function draw(paintColor){
+    if(paintColor == undefined){
+        paintColor = paintBlack;
+    }
+    cell = document.querySelectorAll("#box>div");
+    cell.forEach((div) => {
+        div.addEventListener("mouseover",paintColor);
     });
-});
- /* boxRes = document.getElementById("#input").value; */
+}
+
+function paintBlack(e){
+    e.target.style.backgroundColor = "black";
+}
