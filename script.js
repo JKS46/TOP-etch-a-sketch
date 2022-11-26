f27085b140c6a1426669a053fc077c6717fd7a16
Jaswanth Kishore S <93701274+JKS46@users.noreply.github.com>
2022-11-26 17:31:35 +0530

    box done

--- script.js
+++ script.js
@@ -1,14 +1,26 @@
-const box = document.querySelectorAll("#box>div");
+const cell = document.querySelectorAll("#box>div");
+const box = document.querySelector("#box");
+let row = 25,column = 25;
+for(i=0;i<row;i++){
+    for(j=0;j<column;j++){
+        let div = document.createElement('div');
+        div.classList.add('cell');
+        div.style.width = 100/column + '%';
+        div.style.height = 100/row +'%';
+        div.textContent =" ";
+        box.appendChild(div);
+    }
+}
 function createBox(){
-   
 
 }
 function draw(boxRes){
     
 }
-box.forEach((div) => {
+/* box.forEach((div) => {
     div.addEventListener("hover", () => {
     boxRes = document.getElementById("#input").value;
     draw(boxRes);
     });
 });
+ */
